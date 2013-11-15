90682aaf9bc9759738a810504f1eaa0e18c2bb39
Brian Olsen <brian@velocitywebworks.com>
2013-11-15 16:13:57 -0700

    tweeked cron

--- project/ospi/cron.py
+++ project/ospi/cron.py
@@ -1,20 +1,29 @@
 from .views import *
 from django.utils import timezone
 
+
 def update_log():
     accounts = Account.objects.all()
     for account in accounts:
         status = account.get_status()
         for idx, station in enumerate(status['stations']):
-            mystation = Station.objects.get(number=idx+1, account=account)
-            running = WaterLog.objects.get(end_time__is_null=True, account=account, station=mystation)
-            if station is True and running.exists() is False:
-                #Create Log
-                log = WaterLog.objects.create(account=account, station=mystation, start_time=timezone.now())
+            try:
+                mystation = Station.objects.get(number=idx+1, account=account)
+            except Station.DoesNotExist:
+                continue
+            try:
+                running = WaterLog.objects.get(end_time__is_null=True, account=account, station=mystation)
+            except WaterLog.DoesNotExist:
+                running = False
+            except WaterLog.MultipleObjectsReturned:
+                raise
 
-            elif station is False and running.exists() is True:
+            if station and not running:
+                #Create Log
+                WaterLog.objects.create(account=account, station=mystation, start_time=timezone.now())
+            elif station is False and running:
                 #End log
                 running.end_time = timezone.now()
                 running.save()
-            
+
                 
