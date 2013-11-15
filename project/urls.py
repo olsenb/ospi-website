a9774826dd90a503a8cb28418e4b5435c32a0cd1
Brian Olsen <brian@velocitywebworks.com>
2013-11-15 09:19:42 -0700

    template dirs

--- project/urls.py
+++ project/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url('^ospi/', include('project.ospi.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
