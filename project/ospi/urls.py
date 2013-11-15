dcac48098dfd44a5e30afd4c4267f3b716d27cd2
Randall Tateishi <randall@velocitywebworks.com>
2013-11-15 11:02:23 -0700

    Created basic Views

--- project/ospi/urls.py
+++ project/ospi/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import *
+from .views import *
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url('^$', TemplateView.as_view(template_name="ospi/home.html")),
     url('^zones/$', TemplateView.as_view(template_name="ospi/zones.html")),
-    url('^schedule/$', TemplateView.as_view(template_name="ospi/schedule.html")),
+    url('^zones/create/$', CreateZoneView.as_view(), name="zone_create"),
+    url('^schedule/$', TemplateView.as_view(template_name="ospi/schedule.html"), name="schedule_list"),
+    url('^schedule/create/$', CreateScheduleView.as_view(), name='schedule_create'),
     url('^settings/$', TemplateView.as_view(template_name="ospi/settings.html")),
 )
--- project/ospi/views.py
+++ project/ospi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import CreateView, ListView, UpdateView
 
 # Create your views here.
+class CreateZoneView(CreateView):
+    template_name='ospi/zone_create.html'
+    model=Zone
+
+class CreateScheduleView(CreateView):
+    template_name='ospi/schedule_create.html'
+    model=Schedule
