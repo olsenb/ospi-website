6d9e3868880cf298c21a69669b9fd90dddb95c96
Brian Olsen <brian@velocitywebworks.com>
2013-11-15 13:43:07 -0700

    added station on/off/status

--- project/ospi/tests.py
+++ project/ospi/tests.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+
+from .models import Account, Station
 from .weather import get_current_weather
+
 
 # Create your tests here.
 class GetWeatherTests(TestCase):
@@ -7,3 +11,23 @@ class GetWeatherTests(TestCase):
         result = get_current_weather()
 
         self.assertIsNotNone(result["response"]["current_observation"])
+
+
+class StationTests(TestCase):
+
+    def setUp(self):
+        user = User.objects.create_user("test", "test@test.com")
+        self.account = Account.objects.create(ip="192.168.2.6", port="8080", user=user)
+        for i in range(1, 9):
+            Station.objects.create(account=self.account, number=i)
+
+    def test_stations(self):
+        for station in Station.objects.all():
+            station.enable(60)
+        for station in Station.objects.all():
+            self.assertTrue(station.status)
+            station.disable()
+        for station in Station.objects.all():
+            self.assertFalse(station.status)
+
+
