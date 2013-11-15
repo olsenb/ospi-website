5ee7d2ec170fd443f13073efd32b893e56b6f187
Randall Tateishi <randall@velocitywebworks.com>
2013-11-15 11:19:06 -0700

    Added listviews for Stations and Schedules

--- project/ospi/urls.py
+++ project/ospi/urls.py
@@ -4,9 +4,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url('^$', TemplateView.as_view(template_name="ospi/home.html")),
-    url('^stations/$', TemplateView.as_view(template_name="ospi/stations.html")),
+    url('^stations/$', StationsListView.as_view(), name='stations_list' ),
     url('^stations/create/$', CreateStationsView.as_view(), name="stations_create"),
-    url('^schedule/$', TemplateView.as_view(template_name="ospi/schedule.html"), name="schedule_list"),
+    url('^schedule/$', ScheduleListView.as_view(), name="schedule_list"),
     url('^schedule/create/$', CreateScheduleView.as_view(), name='schedule_create'),
     url('^settings/$', TemplateView.as_view(template_name="ospi/settings.html")),
 )
--- project/ospi/views.py
+++ project/ospi/views.py
@@ -3,9 +3,15 @@ from .models import *
 from django.views.generic import CreateView, ListView, UpdateView
 
 # Create your views here.
+class StationsListView(ListView):
+    model=Station
+
 class CreateStationsView(CreateView):
     template_name='ospi/station_create.html'
     model=Station
+
+class ScheduleListView(ListView):
+    model=Schedule
 
 class CreateScheduleView(CreateView):
     template_name='ospi/schedule_create.html'
