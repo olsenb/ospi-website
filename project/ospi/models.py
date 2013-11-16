9a466f9cce8dc039056ae3cdad9a46b3dbf6ba5e
Mike Moore <mike@velocitywebworks.com>
2013-11-15 22:19:00 -0700

    Adding working turn-on/turn-off buttons.

--- project/ospi/models.py
+++ project/ospi/models.py
@@ -93,7 +93,7 @@ class Station(models.Model):
         response = self.account.send("sn%s" % self.number)
         if response is None:
             return None
-        return bool(int(response))
+        return bool(int(response[-1]))
     @property
     def bit_value(self):
         return pow(2, self.number)
