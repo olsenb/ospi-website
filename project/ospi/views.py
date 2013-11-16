97a9fe652b7642db1f296b456f94c91d1fdc849b
Mike Moore <mike@velocitywebworks.com>
2013-11-15 17:01:15 -0700

    A few updates to the overview page.

--- project/ospi/views.py
+++ project/ospi/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 from .models import *
-from django.views.generic import CreateView, ListView, UpdateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
-# Create your views here.
+
+class HomeView(DetailView):
+    template_name = 'ospi/rpi_home.html'
+
+    def get_object(self, queryset=None):
+        return []
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['stations'] = Station.objects.all()
+        return context
+
+
 class StationsListView(ListView):
     template_name='ospi/stations_view.html'
     model=Station
